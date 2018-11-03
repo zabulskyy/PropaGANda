bc6739e9f91195b12c04e800461bb95a7bbae649
Volodymyr Zabulskyy <v.zabulskyy@gmail.com>
2018-11-03 11:33:26 +0200

    lemma updates

--- scripts/lemma.py
+++ scripts/lemma.py
@@ -1,4 +1,5 @@
-letters = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя-\'"
+if __name__ == "__main__":
+    lemma_dict = get_lemma_dict()
 
 def get_lemma_dict(path="data/lemma_dict.txt"):
     lemma_dict = dict()
@@ -9,35 +10,45 @@ def get_lemma_dict(path="data/lemma_dict.txt"):
             lemma_dict[l[0]] = l[1]
     return lemma_dict
 
-if __name__ == "__main__":
-    lemma_dict = get_lemma_dict()
 
-
-def get_ascii(word, l=letters):
+def get_ascii(word):
+    l = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя-\'"
+    s = "!?.;\"'/\\,;()"
     new = ""
     for w in word:
         if w in l:
             new += w
+        elif w in s and (new and new[-1] != ' '):
+            new += " "
     return new
 
-def get_lemma_word(word, ld=lemma_dict):
+def get_lemma_word(word, ld):
     new_word = get_ascii(word.lower().strip())
-    if new_word and new_word in ld:
-        return ld[new_word]
-    return ""
+    words = [x.strip() for x in new_word.split()]
+    if len(words) <= 1:
+        if new_word and new_word in ld:
+            return [ld[new_word]]
+    else:
+        res = []
+        for word in words:
+            if word and word in ld:
+                res.append(ld[word])
+        return res
+    return [""]
 
-def get_lemma_sent(sent, ld=lemma_dict):
+def get_lemma_sent(sent, ld):
     new = []
     for word in sent.split():
-        word = get_lemma_word(word).strip()
-        if word: 
-            new.append(word)
+        word = get_lemma_word(word, ld)
+        if word and word != [""]: 
+            for w in word:
+                new.append(w)
     return new
 
-def get_lemma_par(par):
+def get_lemma_par(par, ld):
     new = []
     for sent in par.split('.'):
-        sent = get_lemma_sent(sent)
+        sent = get_lemma_sent(sent, ld)
         if sent: 
             new.append(sent)
     return new
